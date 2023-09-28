fd94d33b8d360a023d4c88a773a195f94c9b0ebb
arm-mahesh <armanyamaheshwari@gmail.com>
2023-09-27 22:32:24 -0400

    serverless compute

--- src/server/server.js
+++ src/server/server.js
@@ -17,13 +17,6 @@ const pool = mysql.createPool({
   connectionLimit: 10,
 });
 
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   password: "Crondon123",
-//   connectionLimit: 10,
-// });
-
 pool.on("acquire", (connection) => {
   console.log("Connection %d acquired", connection.threadId);
 });
